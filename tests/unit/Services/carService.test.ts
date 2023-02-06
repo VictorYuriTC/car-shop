affe5cd08d642c8dca50880b24062e5fd6b9b365
Victor Yuri <victoryuritc@yahoo.com>
2023-02-06 03:32:29 -0300

    test: start carService test for create

---
+++ tests/unit/Services/carService.test.ts
@@ -0,0 +1,39 @@
+import sinon from 'sinon';
+import { Model } from 'mongoose';
+import { expect } from 'chai';
+import Car from '../../../src/Domains/Car';
+import ICar from '../../../src/Interfaces/ICar';
+import CarService from '../../../src/Services/CarService';
+
+describe('Test car service', function () {
+  it('should creates a new car when receiving allowed data', async function () {
+    const carInput: ICar = {
+      model: 'Fiesta',
+      year: 1997,
+      color: 'Grey',
+      status: false,
+      buyValue: 3000,
+      doorsQty: 4,
+      seatsQty: 5,
+    };
+
+    const carResponse = {
+      id: '63319d80feb9f483ee823ac5',
+      model: 'Fiesta',
+      year: 1997,
+      color: 'Grey',
+      status: false,
+      buyValue: 3000,
+      doorsQty: 4,
+      seatsQty: 5,
+    };
+
+    const carOutput: Car = new Car(carResponse);
+
+    sinon.stub(Model, 'create').resolves(carOutput);
+
+    const service = new CarService();
+    const result = await service.create(carInput);
+    expect(result).to.be.deep.equal(carOutput);
+  });
+});
