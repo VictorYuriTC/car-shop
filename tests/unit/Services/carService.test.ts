fd239cdb4cdb6453bd28cf2caadd21693820ae1f
Victor Yuri <victoryuritc@yahoo.com>
2023-02-06 05:19:34 -0300

    test: add tests for update car and update motorcycle

--- tests/unit/Services/carService.test.ts
+++ tests/unit/Services/carService.test.ts
@@ -7,8 +7,8 @@ import CarService from '../../../src/Services/CarService';
 
 describe('Test car service layer', function () {
   afterEach(sinon.restore);
-  
-  it('should create a new car when receiving allowed data', async function () {
+
+  it('should create a new car when receiving expected data', async function () {
     const carInput: ICar = {
       model: 'Fiesta',
       year: 1997,
@@ -36,6 +36,37 @@ describe('Test car service layer', function () {
 
     const service = new CarService();
     const result = await service.create(carInput);
+    expect(result).to.be.deep.equal(carOutput);
+  });
+
+  it('should update a car when receiving expected data', async function () {
+    const carInput: ICar = {
+      model: 'Fiesta',
+      year: 1997,
+      color: 'Grey',
+      status: false,
+      buyValue: 3500,
+      doorsQty: 4,
+      seatsQty: 5,
+    };
+
+    const carResponse = {
+      id: '63319d80feb9f483ee823ac5',
+      model: 'Fiesta',
+      year: 1997,
+      color: 'Grey',
+      status: false,
+      buyValue: 3500,
+      doorsQty: 4,
+      seatsQty: 5,
+    };
+
+    const carOutput: Car = new Car(carResponse);
+
+    sinon.stub(Model, 'findByIdAndUpdate').resolves(carOutput);
+
+    const service = new CarService();
+    const result = await service.updateById('63319d80feb9f483ee823ac5', carInput);
     expect(result).to.be.deep.equal(carOutput);
   });
 });
--- tests/unit/Services/motorcycleService.test.ts
+++ tests/unit/Services/motorcycleService.test.ts
@@ -7,8 +7,8 @@ import MotorcycleService from '../../../src/Services/MotorcycleService';
 
 describe('Test motorcycle service layer', function () {
   afterEach(sinon.restore);
-  
-  it('should create a new motorcycle when receiving allowed data', async function () {
+
+  it('should create a new motorcycle when receiving expected data', async function () {
     const motorcycleInput: IMotorcycle = {
       model: 'Honda Cbr 1000rr',
       year: 2011,
@@ -36,6 +36,38 @@ describe('Test motorcycle service layer', function () {
 
     const service = new MotorcycleService();
     const result = await service.create(motorcycleInput);
+    expect(result).to.be.deep.equal(motorcycleOutput);
+  });
+
+  it('should update a Motorcycle when receiving expected data', async function () {
+    const motorcycleInput: IMotorcycle = {
+      model: 'Honda Cb 600f Hornet',
+      year: 2014,
+      color: 'Red',
+      status: true,
+      buyValue: 45.000,
+      category: 'Street',
+      engineCapacity: 600,
+    };
+
+    const motorcycleResponse = {
+      id: '634852326b35b59438fbea2f',
+      model: 'Honda Cb 600f Hornet',
+      year: 2014,
+      color: 'Red',
+      status: true,
+      buyValue: 45.000,
+      category: 'Street',
+      engineCapacity: 600,
+    };
+
+    const motorcycleOutput: Motorcycle = new 
+    Motorcycle(motorcycleResponse);
+
+    sinon.stub(Model, 'findByIdAndUpdate').resolves(motorcycleOutput);
+
+    const service = new MotorcycleService();
+    const result = await service.updateById('634852326b35b59438fbea2f', motorcycleInput);
     expect(result).to.be.deep.equal(motorcycleOutput);
   });
 });
