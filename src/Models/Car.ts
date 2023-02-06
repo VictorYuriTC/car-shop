e6ade706713120ebfaa75bb9d10e36f9b4f6e075
Victor Yuri <victoryuritc@yahoo.com>
2023-02-05 22:06:35 -0300

    feat: add Car model with create method

---
+++ src/Models/Car.ts
@@ -0,0 +1,32 @@
+import {
+  Model,
+  Schema,
+  model,
+  models,
+} from 'mongoose';
+import ICar from '../Interfaces/ICar';
+
+class Car {
+  private schema: Schema;
+  private model: Model<ICar>;
+
+  constructor() {
+    this.schema = new Schema<ICar>({
+      id: { type: String, required: true },
+      model: { type: String, required: true },
+      year: { type: Number, required: true },
+      color: { type: String, required: true },
+      status: { type: Boolean, required: false, default: false },
+      buyValue: { type: Number, required: true },
+      doorsQty: { type: Number, required: true },
+      seatsQty: { type: Number, required: true },
+    });
+    this.model = models.Car || model('Car', this.schema);
+  }
+
+  public async create(car: ICar): Promise<ICar> {
+    return this.model.create({ ...car });
+  }
+}
+
+export default Car;
